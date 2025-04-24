2c1421bdcef3095a2373539d9c863eddca70f28a
ramaz <ramazanovshaxzodbek@gmail.com>
2025-04-24 18:20:32 +0500

    start

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,67 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useEffect, useState, useRef } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [navScrolled, setNavScrolled] = useState(false);
+    const landingRef = useRef(null);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    useEffect(() => {
+        const handleScroll = () => {
+            const landingBottom = landingRef.current?.getBoundingClientRect().bottom;
+
+            if (landingBottom <= 0) {
+                setNavScrolled(true);
+            } else {
+                setNavScrolled(false);
+            }
+        };
+
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
+
+    return (
+        <div className="wrapper">
+            <div className="landing" ref={landingRef}>
+                <header className={`nav ${navScrolled ? 'scrolled' : 'default'}`}>
+                    <div className="logo">TripTour</div>
+                    <nav>
+                        <ul>
+                            <li>Горящие туры</li>
+                            <li>Оплата</li>
+                            <li>О нас</li>
+                            <li>Услуги</li>
+                            <li>Направления</li>
+                            <li>Учебный центр</li>
+                            <li>Контакты</li>
+                        </ul>
+                    </nav>
+                    <div className="lang-switch">
+                        <span>UZ</span>
+                        <span className="active">RU</span>
+                        <span>EN</span>
+                    </div>
+                </header>
+
+                <div className="hero">
+                    <h1>Путешествия по всему миру</h1>
+                    <p>Откройте мир с ********</p>
+                    <p className="p-text">Качественные и надежные туристические услуги с 2021 года</p>
+                    <h2>+998 99 999 99 99 </h2>
+                    <div className="buttons">
+                        <button className="primary">Планировать путешествие</button>
+                        <button className="secondary">Контакты</button>
+                    </div>
+                </div>
+            </div>
+
+
+            
+            <div>
+
+            </div>
+        </div>
+    );
 }
 
-export default App
+export default App;
